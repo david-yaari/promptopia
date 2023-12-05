e6d09f62c42a441a2f9210776d57a8b35b48085b
David Yaari <david,yaari@icloud.com>
2023-12-05 17:17:10 +0200

    SECRET added to route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -26,6 +26,7 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
     }),
   ],
+  secret: process.env.SECRET, // To be added
   callbacks: {
     async session({ session }: { session: Session }) {
       const sessionUser = await User.findOne({
